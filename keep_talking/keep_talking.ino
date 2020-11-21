9024833179f0a50d38ede97afd26b996f41f11e7
Irina Brovar <iris-arcus@yandex.ru>
2020-11-21 16:50:52 +0100

    Merge maze module into the main code

--- keep_talking/keep_talking.ino
+++ keep_talking/keep_talking.ino
@@ -1,8 +1,9 @@
 #include <Wire.h>
-#include <Adafruit_GFX.h>
+#include "Adafruit_GFX.h"
 #include "Adafruit_LEDBackpack.h"
 
 Adafruit_7segment timerDisplay = Adafruit_7segment();
+Adafruit_8x8matrix mazeDisplay = Adafruit_8x8matrix();
 
 int inputDataPin = 5;
 int inputClockPin = 6;
@@ -16,7 +17,40 @@ unsigned long startMs;
 int maxCounterSecs = 180;
 int lastTimerSecs = 0;
 bool explodedBlinkState = true;
- 
+
+// Structs and static constants for the Maze module
+static const uint8_t PROGMEM border[] = {
+  0b11111111,
+  0b10000001,
+  0b10000001,
+  0b10000001,
+  0b10000001,
+  0b10000001,
+  0b10000001,
+  0b11111111,
+};
+
+struct Maze {
+  uint8_t id1;
+  uint8_t id2;
+  uint8_t maze[];
+};
+
+static const Maze MAZES[] = {
+  Maze{
+    0x10, 0x25,
+    {
+      0b11000101, 0b01101100, 0b01010111,
+      0b10101100, 0b00111001, 0b01010110,
+      0b10101001, 0b01101100, 0b01010010,
+      0b10101101, 0b00010011, 0b11010010,
+      0b10000101, 0b01101100, 0b01111010,
+      0b10010111, 0b10010011, 0b11010011,
+    }
+  },
+};
+
+// --- Main functions ---
 void setup() {
   Serial.begin(9600);
   
@@ -31,6 +65,11 @@ void setup() {
   timerDisplay.begin(0x77);
 
   startMs = millis();
+
+  // Maze module setup
+  mazeDisplay.begin(0x71);
+  mazeDisplay.setBrightness(0);
+  validateMaze(MAZES[0]);
 }
 
 void loop() {
@@ -38,8 +77,6 @@ void loop() {
     
   delay(10);
 }
-
-byte buttonState = 1;
 
 void handleModules() {
   digitalWrite(inputClockPin, HIGH);
@@ -55,9 +92,14 @@ void handleModules() {
     if (moduleId == 0) {
       break;
     }
+    // Add modules here
     switch (moduleId) {
       case 1: {  // Timer
-        handleTimer();
+        handleTimer(value);
+        break;
+      }
+      case 2: {  // Maze
+        handleMaze();
         break;
       }
       default:
@@ -69,7 +111,10 @@ void handleModules() {
   digitalWrite(outputLatchPin, HIGH);
 }
 
-void handleTimer() {
+// --- Timer handling functions ---
+byte buttonState = 1;
+
+void handleTimer(byte value) {
   // Reset button
   byte newButtonState = (value >> 4) & 1;
   if (!newButtonState && buttonState) { // Button is just pressed
@@ -115,4 +160,55 @@ void handleTimer() {
     }
   }
   timerDisplay.writeDisplay();
+}
+
+// --- Maze handling functions ---
+
+// Auxilary maze functions
+uint8_t idToX(uint8_t id) {
+  return id % 16;
+}
+uint8_t idToY(uint8_t id) {
+  return id / 16;
+}
+uint8_t pointToId(uint8_t x, uint8_t y) {
+  return x + y * 16;
+}
+
+// Main maze functions
+void drawMaze(const Maze& m){
+  mazeDisplay.clear();
+  mazeDisplay.drawBitmap(0, 0, border, 8, 8, LED_ON);
+  mazeDisplay.drawPixel(idToX(m.id1)+1, idToY(m.id1)+1, LED_ON);
+  mazeDisplay.drawPixel(idToX(m.id2)+1, idToY(m.id2)+1, LED_ON);
+}
+
+void drawMazePoints(uint8_t start_x, uint8_t start_y, uint8_t end_x, uint8_t end_y) {
+  start_x++;
+  start_y++;
+  end_x++;
+  end_y++;
+  
+  unsigned long nowMs = millis();
+  int period = 500;
+  int onTime = 250;
+  if (nowMs % period < onTime) {
+    mazeDisplay.drawPixel(start_x, start_y, LED_ON);
+  } else {
+    mazeDisplay.drawPixel(start_x, start_y, LED_OFF);
+  }
+  mazeDisplay.drawPixel(end_x, end_y, LED_ON);
+  mazeDisplay.writeDisplay();
+}
+
+void validateMaze(const Maze& m) {
+  return;
+}
+
+void handleMaze() {
+  drawMaze(MAZES[0]);
+  uint8_t start_x = 0, start_y = 0;
+  uint8_t end_x = 5, end_y = 5;
+  drawMazePoints(start_x, start_y, end_x, end_y);
+  mazeDisplay.writeDisplay();
 }
